9ad63fac759ea1a13a7dcf149416db9eab19ce81
Siarhei Neparka <fume838@gmail.com>
2024-03-07 21:12:48 +0300

    dels from main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 def lazy_function(a, b):
     "blablabla."
 
-    print(a, b)
-
-
-def new():
-    print('купить продукты!')
-
-
-def my_feature_function():
-    """makes some things."""
-
     size = 44
     price = 19
 
